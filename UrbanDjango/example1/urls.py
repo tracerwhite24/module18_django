8a3c47e51518b09be29fa67a8e9bb66c83194011
tracerwhite <tracerwhite@gmail.com>
2024-12-24 23:01:33 +0300

    Все файлы кроме виртуального окружения

--- UrbanDjango/example1/urls.py
+++ UrbanDjango/example1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UrbanDjango project.
+URL configuration for UrbanDjango9 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from UrbanDjango.task2 import urls as task2_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(task2_urls)),
 ]
+
--- UrbanDjango/manage.py
+++ UrbanDjango/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UrbanDjango.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UrbanDjango9.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
