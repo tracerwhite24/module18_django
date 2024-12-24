15f000b8286f1854bc640a0cc7575d67262367aa
tracerwhite <tracerwhite@gmail.com>
2024-12-24 23:38:22 +0300

    Все файлы кроме виртуального окружения

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UrbanDjango9 project.
+URL configuration for UrbanDjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-from UrbanDjango.task2 import urls as task2_urls
+from django.urls import path
+from . import views  # Импортируйте ваши представления
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(task2_urls)),
+
 ]
+
+
 
